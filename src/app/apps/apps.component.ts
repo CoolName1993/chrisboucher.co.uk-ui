5d143940f430d7a9dae9fc94c1c06c17e24956ef
Chris Boucher <chrisboucher@quantexa.com>
2018-10-11 13:28:39 +0100

    Scrapped basic webgl and started on pixijs

--- src/app/apps/apps.component.ts
+++ src/app/apps/apps.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { mat4 } from 'gl-matrix';
-
-/// <reference path="webgl2/webgl2.d.ts" />
-/// <reference path="gl-matrix/gl-matrix.d.ts" />
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-apps',
@@ -14,298 +11,41 @@ export class AppsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.main();
-  }
-
-  main() {
-    const canvas = document.querySelector<HTMLCanvasElement>('#glCanvas');
-    // Initialize the GL context
-    const gl = canvas.getContext('webgl');
-
-    // Only continue if WebGL is available and working
-    if (gl === null) {
-      alert('Unable to initialize WebGL. Your browser or machine may not support it.');
-      return;
+    const app = new PIXI.Application({
+      autoResize: true,
+      resolution: devicePixelRatio 
+    });
+    document.querySelector('#frame').appendChild(app.view);
+    
+    // Lets create a red square, this isn't 
+    // necessary only to show something that can be position
+    // to the bottom-right corner
+    const rect = new PIXI.Graphics()
+      .beginFill(0xf50057)
+      .drawCircle(-200, -200, 50);
+      
+    // Add it to the stage
+    app.stage.addChild(rect);
+    
+    // Listen for window resize events
+    window.addEventListener('resize', resize);
+    
+    // Resize function window
+    function resize() {
+    
+      // Get the p
+      const parent = app.view.parentElement;
+       
+      // Resize the renderer
+      app.renderer.resize(parent.clientWidth, parent.clientHeight);
+      
+      // You can use the 'screen' property as the renderer visible
+      // area, this is more useful than view.width/height because
+      // it handles resolution
+      rect.position.set(app.screen.width, app.screen.height);
     }
-
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    // Vertex shader program
-
-    const vsSource = `
-    attribute vec4 aVertexPosition;
-    attribute vec4 aVertexColor;
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
-    varying lowp vec4 vColor;
-    void main(void) {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-      vColor = aVertexColor;
-    }
-  `;
-
-    // Fragment shader program
-
-    const fsSource = `
-    varying lowp vec4 vColor;
-    void main(void) {
-      gl_FragColor = vColor;
-    }
-  `;
-
-    // Initialize a shader program; this is where all the lighting
-    // for the vertices and so forth is established.
-    const shaderProgram = this.initShaderProgram(gl, vsSource, fsSource);
-
-    // Collect all the info needed to use the shader program.
-    // Look up which attributes our shader program is using
-    // for aVertexPosition, aVevrtexColor and also
-    // look up uniform locations.
-    const programInfo = {
-      program: shaderProgram,
-      attribLocations: {
-        vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
-        vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
-      },
-      uniformLocations: {
-        projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-        modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
-      },
-    };
-
-    // Here's where we call the routine that builds all the
-    // objects we'll be drawing.
-    const buffers = this.initBuffers(gl);
-
-    // Draw the scene
-    this.drawScene(gl, programInfo, buffers);
-  }
-
-  //
-  // initBuffers
-  //
-  // Initialize the buffers we'll need. For this demo, we just
-  // have one object -- a simple two-dimensional square.
-  //
-  initBuffers(gl) {
-
-    // Create a buffer for the square's positions.
-
-    const positionBuffer = gl.createBuffer();
-
-    // Select the positionBuffer as the one to apply buffer
-    // operations to from here out.
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // Now create an array of positions for the square.
-
-    const positions = [
-      1.0, 1.0,
-      -1.0, 1.0,
-      1.0, -1.0,
-      -1.0, -1.0,
-    ];
-
-    // Now pass the list of positions into WebGL to build the
-    // shape. We do this by creating a Float32Array from the
-    // JavaScript array, then use it to fill the current buffer.
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-    // Now set up the colors for the vertices
-
-    const colors = [
-      1.0, 1.0, 1.0, 1.0,    // white
-      1.0, 0.0, 0.0, 1.0,    // red
-      0.0, 1.0, 0.0, 1.0,    // green
-      0.0, 0.0, 1.0, 1.0,    // blue
-    ];
-
-    const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-    return {
-      position: positionBuffer,
-      color: colorBuffer,
-    };
-  }
-
-  //
-  // Draw the scene.
-  //
-  drawScene(gl, programInfo, buffers) {
-    this.resize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-    gl.clearDepth(1.0);                 // Clear everything
-    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
-
-    // Clear the canvas before we start drawing on it.
-
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.clear(gl.DEPTH_BUFFER_BIT);
-
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
-
-    const fieldOfView = 45 * Math.PI / 180;   // in radians
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 100.0;
-    const projectionMatrix = mat4.create();
-
-    // note: glmatrix.js always has the first argument
-    // as the destination to receive the result.
-    mat4.perspective(projectionMatrix,
-      fieldOfView,
-      aspect,
-      zNear,
-      zFar);
-
-    // Set the drawing position to the "identity" point, which is
-    // the center of the scene.
-    const modelViewMatrix = mat4.create();
-
-    // Now move the drawing position a bit to where we want to
-    // start drawing the square.
-
-    mat4.translate(modelViewMatrix,     // destination matrix
-      modelViewMatrix,     // matrix to translate
-      [-0.0, 0.0, -6.0]);  // amount to translate
-
-    // Tell WebGL how to pull out the positions from the position
-    // buffer into the vertexPosition attribute
-    {
-      const numComponents = 2;
-      const type = gl.FLOAT;
-      const normalize = false;
-      const stride = 0;
-      const offset = 0;
-      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-      gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexPosition,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-      gl.enableVertexAttribArray(
-        programInfo.attribLocations.vertexPosition);
-    }
-
-    // Tell WebGL how to pull out the colors from the color buffer
-    // into the vertexColor attribute.
-    {
-      const numComponents = 4;
-      const type = gl.FLOAT;
-      const normalize = false;
-      const stride = 0;
-      const offset = 0;
-      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-      gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexColor,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-      gl.enableVertexAttribArray(
-        programInfo.attribLocations.vertexColor);
-    }
-
-    // Tell WebGL to use our program when drawing
-
-    gl.useProgram(programInfo.program);
-
-    // Set the shader uniforms
-
-    gl.uniformMatrix4fv(
-      programInfo.uniformLocations.projectionMatrix,
-      false,
-      projectionMatrix);
-    gl.uniformMatrix4fv(
-      programInfo.uniformLocations.modelViewMatrix,
-      false,
-      modelViewMatrix);
-
-    {
-      const offset = 0;
-      const vertexCount = 4;
-      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-    }
-  }
-
-  //
-  // Initialize a shader program, so WebGL knows how to draw our data
-  //
-  initShaderProgram(gl, vsSource, fsSource) {
-    const vertexShader = this.loadShader(gl, gl.VERTEX_SHADER, vsSource);
-    const fragmentShader = this.loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-    // Create the shader program
-
-    const shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-
-    // If creating the shader program failed, alert
-
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-      alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-      return null;
-    }
-
-    return shaderProgram;
-  }
-
-  //
-  // creates a shader of the given type, uploads the source and
-  // compiles it.
-  //
-  loadShader(gl, type, source) {
-    const shader = gl.createShader(type);
-
-    // Send the source to the shader object
-
-    gl.shaderSource(shader, source);
-
-    // Compile the shader program
-
-    gl.compileShader(shader);
-
-    // See if it compiled successfully
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-      gl.deleteShader(shader);
-      return null;
-    }
-
-    return shader;
-  }
-
-  resize(canvas) {
-    // Lookup the size the browser is displaying the canvas.
-    const displayWidth = canvas.clientWidth;
-    const displayHeight = canvas.clientHeight;
-
-    // Check if the canvas is not the same size.
-    if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
-      // Make the canvas the same size
-      canvas.width = displayWidth;
-      canvas.height = displayHeight;
-    }
+    
+    resize();
   }
 
 }
